03b33dc61e59e35b28d5432dac62dec189a38cd2
Dmitriy <devilins24@gmail.com>
2022-02-09 16:57:39 +0200

    add theme auto switch on device/browser theme preference

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const toggle = document.querySelector("#theme-switcher");
 toggle.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });
+
+// Auto enable dark mode if device/browser is set to dark theme
+const useDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+
+const toggleDarkMode = (state) => {
+  document.body.classList.toggle("dark", state);
+  // turn on dark mode ui switch
+  toggle.checked = true;
+};
+toggleDarkMode(useDarkMode.matches);
+useDarkMode.addEventListener((evt) => toggleDarkMode(evt.matches));
