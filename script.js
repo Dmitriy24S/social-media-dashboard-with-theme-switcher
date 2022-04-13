67938add6c5e8cdb694c7ce0d6956a16924a5ada
Dmitri <dmitri@MacBook-Air-D.local>
2022-04-13 11:38:31 +0300

    adjust dark mode js, style adjustments, remove transitions

--- script.js
+++ script.js
@@ -4,13 +4,27 @@ toggle.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });
 
-// Auto enable dark mode if device/browser is set to dark theme
-const useDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+// Auto enable dark mode if device/browser is set to dark theme:
 
+// MediaQueryList object
+const useDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+// MediaQueryList {media: (prefers-color-scheme: dark)', matches: false,
+
+// Toggle the dark mode class if media quary matches
 const toggleDarkMode = (state) => {
   document.body.classList.toggle("dark", state);
-  // turn on dark mode ui switch
-  toggle.checked = true;
+  // toggle theme switch
+  if (state) {
+    toggle.checked = false;
+  } else {
+    toggle.checked = true;
+  }
 };
+
+// Innital setting - true/false
 toggleDarkMode(useDarkMode.matches);
-useDarkMode.addEventListener((evt) => toggleDarkMode(evt.matches));
+
+// Listen for chanes in the OS settings
+useDarkMode.addEventListener("change", (evt) => {
+  toggleDarkMode(evt.matches);
+});
